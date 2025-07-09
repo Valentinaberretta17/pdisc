c77ebee5c648fa086be60ddc297b7606b7f88112
Valentinaberretta17 <valentinaberretta4@gmail.com>
2025-07-08 21:21:54 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main{
+public static void main(string[] args){
+pelicula p =new pelicula("la la land","damien chazelle",128);
+p.mostrarinfo();
+pelicula2 = new pelicula("interestelar","christopher nolan");
+p2.mostrarinfo();
+pelicula3= new pelicula("the notebook",121);
+p3.mostrarinfo();
+    }
+}
---
+++ pelicula.java
@@ -0,0 +1,21 @@
+public class pelicula {
+private string titulo;
+private string director;
+private int duracion;
+public pelicula(string titulo,string director,int duracion){
+this.titulo=titulo;
+this.director=director,
+this.duracion=duracion;
+}
+public pelicula(strin titulo,sting director){
+this.titulo=titulo;
+this.director=director;
+}
+public pelicula(string titulo,int duracion){
+this.titulo=titulo;
+this.duracion=duracion;
+}
+public void mostrarinfo(){
+system.out.println("la pelicula es: "+ titulo+ del director: "+director+" y dura: "+duracion);
+{
+{
